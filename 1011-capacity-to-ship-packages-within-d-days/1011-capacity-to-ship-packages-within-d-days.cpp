d7aa061eb5eeab937c92d847086dc279af66cf74
prabhsimrat28 <155677348+prabhsimrat28@users.noreply.github.com>
2026-06-27 17:04:07 +0530

    Time: 16 ms (20.74%), Space: 35.2 MB (19.12%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+
+    bool valid(int &mid,int maxdays,vector<int> &nums)
+    {
+        int cur=1;
+        int sum=0;
+        for(int i:nums)
+        {
+            if(i+sum>mid)
+            {
+                cur++;
+                sum=0;
+            }
+
+            sum=sum+i;
+            
+        }
+        return maxdays>=cur;
+    }
+
+
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int sum=0,maxi=0;
+        for(int i:weights)
+        {
+            sum=sum+i;
+            maxi=max(maxi,i);
+        }
+        int low=maxi,high=sum;
+        
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            cout<<mid<<endl;
+            if(valid(mid,days,weights))
+            {
+                sum=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return sum;
+    }
+};
