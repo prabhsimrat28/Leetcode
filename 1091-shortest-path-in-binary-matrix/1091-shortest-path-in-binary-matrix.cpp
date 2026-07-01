3a06b503b149f564ba5c98304d30d14704d05084
prabhsimrat28 <155677348+prabhsimrat28@users.noreply.github.com>
2026-07-02 01:00:41 +0530

    Time: 14 ms (75.54%), Space: 24.6 MB (68.51%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -10,36 +10,31 @@ public:
         if(n==1)
         return 1;
         
-        int d=1;
         vector<int> x={1,0,1,-1,0,-1,1,-1};
         vector<int> y={1,1,0,0,-1,-1,-1,1};
-        queue<pair<int,int>> q;
-        q.push({0,0});
+        queue<pair<int,pair<int,int>>> q;
+        q.push({1,{0,0}});
         grid[0][0]=1;
         while(!q.empty())
         {
-            int N=q.size();
-            while(N--)
+            int i=q.front().second.first;
+            int j=q.front().second.second;
+            int d=q.front().first;
+            q.pop();
+            for(int k=0;k<8;k++)
             {
-                int i=q.front().first;
-                int j=q.front().second;
-                q.pop();
-                for(int k=0;k<8;k++)
+                int i1=i+x[k];
+                int j1=j+y[k];
+                if(i1==n-1 && j1==n-1)
                 {
-                    int i1=i+x[k];
-                    int j1=j+y[k];
-                    if(i1==n-1 && j1==n-1)
-                    {
-                        return d+1;
-                    }
-                    if(i1>=0 && i1<n && j1>=0 && j1<n && grid[i1][j1] == 0)
-                    {
-                        q.push({i1,j1});
-                        grid[i1][j1]=1;
-                    }
+                   return d+1;
+                }
+                if(i1>=0 && i1<n && j1>=0 && j1<n && grid[i1][j1] == 0)
+                {
+                    q.push({d+1,{i1,j1}});
+                    grid[i1][j1]=1;
                 }
             }
-            d++;
         }
         return -1;
     }
