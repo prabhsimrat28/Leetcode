639aba71373aba0d98a2a7f848dd7be3653ff33a
prabhsimrat28 <155677348+prabhsimrat28@users.noreply.github.com>
2026-07-09 22:44:50 +0530

    Time: 0 ms (100%), Space: 11.6 MB (76.84%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,4 +1,35 @@
-class Solution {
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int n=s.size();
+//         if(n==0||n==1)
+//         {
+//             return n;
+//         }
+//         unordered_map<char,int> occ;
+//         occ[s[0]]=0;
+//         int i=0,j=1;
+//         int len=0;
+//         while(j<n&&i<n)
+//         {
+//             if(occ.find(s[j])!=occ.end())
+//             {
+//                 while(i<occ[s[j]]+1)
+//                 {
+//                     occ.erase(s[i]);
+//                     i++;
+//                 }
+//             }
+//             len=max(len,j-i+1);
+//             occ[s[j]]=j;
+//             j++;
+//         }
+//         return len;
+//     }
+// };
+
+
+ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n=s.size();
@@ -6,24 +37,21 @@ public:
         {
             return n;
         }
-        unordered_map<char,int> occ;
-        occ[s[0]]=0;
-        int i=0,j=1;
         int len=0;
+        
+        vector<int> mp(256,-1);
+        int i=0,j=1;
+        mp[s[0]]=0;
         while(j<n&&i<n)
         {
-            if(occ.find(s[j])!=occ.end())
+            if(mp[s[j]]>=i)
             {
-                while(i<occ[s[j]]+1)
-                {
-                    occ.erase(s[i]);
-                    i++;
-                }
+                i=mp[s[j]]+1;
             }
+            mp[s[j]]=j;
             len=max(len,j-i+1);
-            occ[s[j]]=j;
             j++;
         }
         return len;
     }
-};
+ };
