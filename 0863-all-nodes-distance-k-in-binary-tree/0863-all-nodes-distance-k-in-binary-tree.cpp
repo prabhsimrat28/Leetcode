e421690daa9a69f801e72fccdca3d2d5a2c25cfd
prabhsimrat28 <155677348+prabhsimrat28@users.noreply.github.com>
2026-07-30 01:51:23 +0530

    Time: 9 ms (20.26%), Space: 15.5 MB (49.6%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        if(k==0)
+        return {target->val};
+        unordered_map<TreeNode *,TreeNode *> par;
+        queue<TreeNode *> q;
+        q.push(root);
+        par[root]=NULL;
+        while(!q.empty())
+        {
+            TreeNode *parent=q.front();
+            q.pop();
+            if(parent->left)
+            {
+                q.push(parent->left);
+                par[parent->left]=parent;
+            }
+            if(parent->right)
+            {
+                q.push(parent->right);
+                par[parent->right]=parent;
+            }
+        }
+
+        queue<TreeNode *> targetnode;
+        targetnode.push(target);
+        unordered_set<TreeNode *> visited;
+        visited.insert(target);
+        
+        
+        while(!targetnode.empty())
+        {
+            int s=targetnode.size();
+            for(int i=0;i<s;i++)
+            {
+                TreeNode *node=targetnode.front();
+                targetnode.pop();
+                if(node->left && !visited.count(node->left))
+                {
+                    targetnode.push(node->left);
+                    visited.insert(node->left);
+                }
+                if(node->right && !visited.count(node->right))
+                {
+                    targetnode.push(node->right);
+                    visited.insert(node->right);
+                }
+                TreeNode *p=par[node];
+                if(p && !visited.count(p))
+                {
+                    targetnode.push(p);
+                    visited.insert(p);
+                }
+            }
+            k--;
+            if(k==0)
+            break;
+        }
+
+        vector<int> ans;
+        while(!targetnode.empty())
+        {
+            ans.push_back(targetnode.front()->val);
+            targetnode.pop();
+        }
+        return ans;
+    }
+};
