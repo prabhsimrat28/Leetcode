9a002db0ea9311d55cfb475fe5ea78ee18ed0aa5
prabhsimrat28 <155677348+prabhsimrat28@users.noreply.github.com>
2026-08-01 00:59:07 +0530

    Time: 8 ms (61.48%), Space: 25.4 MB (84.45%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,33 +1,51 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        unordered_map<char,int> mp;
+        // unordered_map<char,int> mp;
+        // for(char c:word)
+        // {
+        //     mp[c]++;
+        // }
+
+        // int sum=0;
+        // priority_queue<int> pq;
+        // for(auto it:mp)
+        // {
+        //     pq.push(it.second);
+        // }
+
+        // int multiplier=1;
+        // int i=2;
+        // while(!pq.empty())
+        // {
+        //     int freq=pq.top();
+        //     pq.pop();
+        //     if(i>9)
+        //     {
+        //         i=2;
+        //         multiplier++;
+        //     }
+        //     sum=sum+multiplier*freq;
+        //     i++;
+        // }
+
+        vector<int> freq(26,0);
         for(char c:word)
         {
-            mp[c]++;
+            freq[c-'a']++;
         }
+
+        sort(freq.begin(),freq.end(),greater<int>());
 
         int sum=0;
-        priority_queue<int> pq;
-        for(auto it:mp)
+        for(int i=0;i<freq.size();i++)
         {
-            pq.push(it.second);
+            sum=sum+freq[i]*((i/8)+1);
         }
+        return sum;
 
-        int multiplier=1;
-        int i=2;
-        while(!pq.empty())
-        {
-            int freq=pq.top();
-            pq.pop();
-            if(i>9)
-            {
-                i=2;
-                multiplier++;
-            }
-            sum=sum+multiplier*freq;
-            i++;
-        }
+
+
         return sum;
     }
 };
